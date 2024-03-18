bb8dfecc7c8007a324ad804d83cb1529a2b97327
Kanchan Pandey <162719920+Kanchanpandey027@users.noreply.github.com>
2024-03-18 19:22:18 +0530

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    char name [20]="ram sharma";
+    char address[20]= "Lucknow";
+    cout <<"name : "<< name << endl << "address :" << address << endl ;
+    return 0;
+}
