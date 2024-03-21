701be31f50f62012cd451670d2ea6e20f963e2ba
Kanchan Pandey <162719920+Kanchanpandey027@users.noreply.github.com>
2024-03-21 19:27:31 +0530

    Add files via upload

---
+++ main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+   printf("########\n#\n#\n#####\n#\n#\n#\n\n");
+   printf("  #####\n");
+   printf("##     ##\n");
+   printf("#\n");
+   printf("#\n");
+   printf("#\n");
+   printf("#\n");
+   printf("#\n");
+   printf("##     ##\n");
+   printf("  #####\n");
+}
