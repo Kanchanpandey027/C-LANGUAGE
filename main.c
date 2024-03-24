dd381fcecc7a618c10ebd20d7912d30c8e241e18
Kanchan Pandey <162719920+Kanchanpandey027@users.noreply.github.com>
2024-03-24 13:39:25 +0530

    Add files via upload

--- main.c
+++ main.c
@@ -3,11 +3,10 @@
 
 int main()
 {
-    int h=7;
-    int w=5;
-    int p=2*(h+w);
-    int a=h*w;
-    printf("perimeter of rectangle = %d inches\n",p);
-    printf("area of rectangle =%d sq inches",a);
+    float r=6;
+    float c=2*3.14*r;
+    float a=3.14*r*r;
+    printf("perimeter of circle=%f inches\n",c);
+    printf("area of circle=%f inches",a);
 
 }
