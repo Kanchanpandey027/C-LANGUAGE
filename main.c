8a52f8ecda4ea9bb9a0cfa9099ecbf30e87cb9d8
Kanchan Pandey <162719920+Kanchanpandey027@users.noreply.github.com>
2024-03-27 23:04:40 +0530

    Add files via upload

--- main.c
+++ main.c
@@ -3,12 +3,18 @@
 
 int main()
 {
-    int days=1329;
-    int year=days/365;
-    int week=(days%365)/7;
-    int day=(days/365);
-    printf("year=%d\n",year);
-    printf("week=%d\n",week);
-    printf("day =%d\n",day);
-
+  int id;
+  int salary;
+  int totalsalary;
+  int hr;
+  totalsalary=salary*hr;
+  printf("enter employeeid :");
+  scanf("%d",&id);
+  printf("enter salary:");
+  scanf("%d",&salary);
+  printf("enter hr :");
+  scanf("%d",&hr);
+  printf("id=%d\n",id);
+  printf("total salary=%d\n",totalsalary=salary*hr);
+    return 0;
 }
