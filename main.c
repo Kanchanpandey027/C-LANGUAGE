9970b39f9f065e22b16b26fe359d7c6e5202b258
Kanchan Pandey <162719920+Kanchanpandey027@users.noreply.github.com>
2024-03-29 22:49:16 +0530

    Add files via upload

--- main.c
+++ main.c
@@ -3,14 +3,20 @@
 
 int main()
 {
-    float totaldistance;
-    float fuelspent;
-    float avg;
-    avg=totaldistance/fuelspent;
-    printf("enter totaldistance:");
-    scanf("%f",&totaldistance);
-    printf("enter fuelspent:");
-    scanf("%f",&fuelspent);
-    printf("avg=%f",avg=totaldistance/fuelspent);
+    int amount,n100=0,n50=0,n20=0,n5=0;
+    printf("enter your amount:");
+    scanf("%d",&amount);
+    n100=amount/100;
+    printf("%d notes of 100\n",n100);
+    amount=amount-(n100*100);
+    n50=amount/50;
+    printf("%d notes of 50\n",n50);
+    n20=amount/20;
+    printf("%d notes of 20\n",n20);
+    n5=amount/5;
+    printf("%d notes of 5\n",n5);
+
+
+
     return 0;
 }
