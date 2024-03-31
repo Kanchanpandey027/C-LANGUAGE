856fdc63acf967902183d69497e96b95481b5d37
Kanchan Pandey <162719920+Kanchanpandey027@users.noreply.github.com>
2024-03-31 23:41:11 +0530

    Add files via upload

--- main.c
+++ main.c
@@ -3,20 +3,18 @@
 
 int main()
 {
-    int amount,n100=0,n50=0,n20=0,n5=0;
-    printf("enter your amount:");
-    scanf("%d",&amount);
-    n100=amount/100;
-    printf("%d notes of 100\n",n100);
-    amount=amount-(n100*100);
-    n50=amount/50;
-    printf("%d notes of 50\n",n50);
-    n20=amount/20;
-    printf("%d notes of 20\n",n20);
-    n5=amount/5;
-    printf("%d notes of 5\n",n5);
+    int sec;
+    int hr;
+    int min;
+    min=min/60;
+    sec=sec%60;
+    hr=sec/3600;
 
-
+    printf("enter your sec:");
+    scanf("%d",&sec);
+    printf("min %d\n",min=sec/60);
+    printf("sec %d\n",sec=sec%60);
+    printf("hr %d",hr=sec%60);
 
     return 0;
 }
