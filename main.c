b01e62be7ff929d7d7c91703adf9861fe9e15d04
Kanchan Pandey <162719920+Kanchanpandey027@users.noreply.github.com>
2024-04-01 23:54:45 +0530

    Add files via upload

--- main.c
+++ main.c
@@ -3,18 +3,13 @@
 
 int main()
 {
-    int sec;
-    int hr;
-    int min;
-    min=min/60;
-    sec=sec%60;
-    hr=sec/3600;
-
-    printf("enter your sec:");
-    scanf("%d",&sec);
-    printf("min %d\n",min=sec/60);
-    printf("sec %d\n",sec=sec%60);
-    printf("hr %d",hr=sec%60);
-
+    int a,b,t;
+    printf("enter any 2 num:");
+    scanf("%d %d",&a,&b);
+    printf("A=%d B=%d\n",a,b);
+    t=a;
+    a=b;
+    b=t;
+    printf("A= %d B=%d",a,b);
     return 0;
 }
