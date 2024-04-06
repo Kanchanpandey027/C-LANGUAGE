19b14c35378cb4e2c7fbcdb28cec154c9d8141e5
Kanchan Pandey <162719920+Kanchanpandey027@users.noreply.github.com>
2024-04-06 22:54:30 +0530

    Add files via upload

--- main.c
+++ main.c
@@ -3,163 +3,24 @@
 
 int main()
 {
-    int a[3][3];
-    int b[3][3];
-    int c[3][3];
-    int i,j,num;
-    printf("enter any even number of first array");
-
-      for(i=0;i<3;i++)
+    int num,i,temp=0;
+    int odd[5];
+    while(1)
     {
-        for (j=0;j<3;j++)
+    printf("Enter your odd number:\n");
+    scanf("%d",&num);
+    if(num%2==1)
     {
-        while(1)
+    odd[i]=num;
+    i++;
+    }
+    if(i==5)
+        break;
+}
+        for(i=0;i<5;i++)
+            temp=temp+odd[5];
         {
-          int num;
-              printf("enter a[%d][%d]= ",i,j);
-             scanf("%d",&num);
-             if(num%2==0)
-             {
-                 a[i][j]=num;
-                 i++;
-                 j++;
-
-             }
-             if(i==3)
-
-
-                break;
-             }
-    }
-    }
-    printf("enter any odd number for second array:\n");
-
-        for(i=0;i<3;i++)
-    {
-        for (j=0;j<3;j++)
-    {
-        while(1){
-           int num;
-            printf("enter b[%d][%d]= ",i,j);
-             scanf("%d",&num);
-             if(num%2!=0)
-             {
-                 b[i][j]=num;
-                 i++;
-                 j++;
-
-             }
-             if(i==3)
-                   break;
-             }
-    }
-    }
-
-   printf("first array is \n");
-    for(i=0;i<3;i++)
-    {
-        for(j=0;j<3;j++)
-        {
-            printf("%d",a[i][j]);
+         printf("sum of all num :%d ",temp);
         }
-        printf("\n");
-    }
-
-
-    printf("second array is:\n");
-    for(i=0;i<3;i++)
-    {
-        for(j=0;j<3;j++)
-        {
-            printf("%d",b[i][j]);
-        }
-        printf("\n");
-    }
-
-             printf("\n\n result\n\n");
-             for(i=0;i<3;i++)
-             {
-                 for(j=0;j<3;j++)
-             {
-                 c[i][j]=a[i][j]+b[i][j];
-                 printf("%d",c[i][j]);
-             }
-             printf("\n");
-             }
-return 0 ;
-    }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    return 0;
+}
