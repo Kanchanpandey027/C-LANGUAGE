33d928ce8ba70c4f18e034a3562e9c6e4cae5b19
Kanchan Pandey <162719920+Kanchanpandey027@users.noreply.github.com>
2024-04-10 23:55:10 +0530

    Add files via upload

--- main.c
+++ main.c
@@ -1,19 +1,64 @@
 #include <stdio.h>
 #include <stdlib.h>
-
 int main()
 {
-   int i,temp=0;
-   int a[4];
-   for(i=0;i<4;i++)
-   {
-       printf("Enter your number :%d\n",i+1);
-       scanf("%d",&a[i]);
+    int a[2][2];
+    int b[2][2];
+    int c[2][2],i,j;
+
+    printf("matrix 1 is :\n");
+    for(i=0;i<2;i++)
+    {
+     for(j=0;j<2;j++)
+    {
+    scanf("%d ",&a[i][j]);
+    }
+    }
+
+    printf("matrix 2 is:\n");
+    for (i=0;i<2;i++)
+    {
+        for (j=0;j<2;j++)
+        {
+            scanf("%d ",&b[i][j]);
+        }
+    }
+    printf("the 1st matrix is:");
+    for (i=0;i<2;i++)
+    {
+        for (j=0;j<2;j++)
+            {
+                printf("%d",a[i][j]);
+            }
+            printf("\n");
+    }
+    printf("the 2nd matrix is:");
+    for( i=0;i<2;i++)
+    {
+        for(j=0;j<2;j++)
+        {
+            printf("%d",b[i][j]);
+        }
+        printf("\n");
+    }
+
+for(i=0;i<2;i++)
+{
+    for (j=0;j<2;j++)
+        {
+         c[i][j]=a[i][j]+b[i][j];
+        }
+    printf("\n");
    }
-   for(i=0;i<4;i++)
-   {
-       temp=temp+a[i];
-   }
-   printf("sum of all element : %d\n",temp);
-    return 0;
-}
+   printf("sum of matrix :\n");
+       for( i=0;i<2;i++)
+    {
+        for(j=0;j<2;j++)
+        {
+            printf("%d",b[i][j]);
+        }
+        printf("\n");
+    }
+return 0;
+
+    }
