94c7d702bfdc4e88f8af2e5cc80d6b14b02935d2
Kanchan Pandey <162719920+Kanchanpandey027@users.noreply.github.com>
2024-04-23 19:31:50 +0530

    Add files via upload

--- main.c
+++ main.c
@@ -3,14 +3,14 @@
 
 int main()
 {
-    int m;
-    printf("enter data:");
-    scanf("%d",&m);
-    if(m>0)
-        printf("the value of n=1");
-     else if(m=0)
-        printf("the value of n=0");
-    else
-         printf("the value of n=-1");
+    int a,b,c;
+    printf("enter 3 number:\n");
+    scanf("%d%d%d",&a,&b,&c);
+    if(a>b)
+        printf("1st number is greater among three");
+    if(b>c)
+        printf("2nd number is greater among three");
+    if(c>a)
+        printf("3rd number is greater among three");
     return 0;
 }
