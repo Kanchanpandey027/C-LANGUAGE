6915e688c80692174e015f12325cb6945cc415f1
Kanchan Pandey <162719920+Kanchanpandey027@users.noreply.github.com>
2024-04-24 22:11:20 +0530

    Add files via upload

--- main.c
+++ main.c
@@ -3,14 +3,44 @@
 
 int main()
 {
-    int a,b,c;
-    printf("enter 3 number:\n");
-    scanf("%d%d%d",&a,&b,&c);
-    if(a>b)
-        printf("1st number is greater among three");
-    if(b>c)
-        printf("2nd number is greater among three");
-    if(c>a)
-        printf("3rd number is greater among three");
-    return 0;
+
+
+int maths;
+int phy;
+int chem;
+int tos=maths+phy+chem;
+int a=maths+tos;
+
+    printf("enter marks obtained in physics:");
+    scanf("%d",&phy);
+    printf("enter marks obtained in maths:");
+    scanf("%d",&maths);
+    printf("enter marks obtained in chem:");
+    scanf("%d",&chem);
+    printf("total in all three sub%d\n",tos=maths+phy+chem);
+    printf("total in a",a=maths+tos);
+    if(phy>=55)
+    {
+        printf("The candidate is eligible for admission");
+    }
+    if (chem>50)
+        {
+         //printf("The candidate is eligible for admission");
+        }
+    if (maths>=65)
+    {
+         //printf("The candidate is eligible for admission");
+    }
+    else if(tos>=180)
+    {
+        printf("The candidate is eligible for admission");
+    }
+    else if(a>=40)
+    {
+        printf("The candidate is eligible for admission");
+    }
+    else {
+        printf("The candidate is not eligible for admission");
+    }
 }
+
