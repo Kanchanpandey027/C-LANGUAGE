893d80b33f2d0ee606b8023eef54b98b03c7721b
Kanchan Pandey <162719920+Kanchanpandey027@users.noreply.github.com>
2024-04-26 23:05:55 +0530

    Add files via upload

--- main.c
+++ main.c
@@ -3,41 +3,20 @@
 
 int main()
 {
-    int rollno;
-    char n,a,m,e,s;
-    float phy;
-    float chem;
-    float comp_app;
-    float total=phy+chem+comp_app;
-    float per;
-    printf("Enter the roll number of student:");
-    scanf("%d",&rollno);
-   // printf("Enter the name of the student:");
-    //scanf("%c %c%c%c%c",&n,&a,&m,&e,&s);
-    printf("Enter the marks of phy:\n");
-    scanf("%f",&phy);
-    printf("Enter the marks of chem:\n");
-    scanf("%f",&chem);
-    printf("Enter the marks of comp_app:\n");
-    scanf("%f",&comp_app);
-    printf("total marks of student:%f\n",total=phy+chem+comp_app);
-    per=total/300*100;
-    printf("percentage:%f\n",per=total/300*100);
-    if(per>70)
-    {
-        printf("Division =First");
-    }
-    else if(per>60)
+int a;
+int sum=0;
+
+printf("the first 10 natural number\n");
+
+for(a=1;a<11;a++)
 {
-    printf("Division =Second");
+    sum=sum+a;
+    printf("%d\n",a);
 }
-  else if(per>49)
-  {
-      printf("Division=Third");
-  }
-  else
-    {
-    printf("Fail");
-  }
+{
+printf("sum is:%d\n",sum);
 }
 
+
+    return 0;
+}
