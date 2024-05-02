bc5a7215ee6482325daa2c1b46b1c3e17a3d61ed
Kanchan Pandey <162719920+Kanchanpandey027@users.noreply.github.com>
2024-05-02 10:50:28 +0530

    Add files via upload

--- main.c
+++ main.c
@@ -2,13 +2,19 @@
 #include <stdlib.h>
 
 int main()
+
 {
-    int a,mul;
-    printf("input the number:\n");
-    scanf("%d",&mul);
-    for(a=1;a<=10;a++)
+    int a,b,m;
+    printf("Input upto the table number  starting from 1 : ");
+    scanf("%d",&m);
+    printf("multiplication table from 1 to %d \n",m);
+    for(b=1;b<=10;b++)
     {
-        printf("15*%d=%d\n",a,mul*a);
+    for(a=1;a<=m;a++)
+        {
+    printf("%d*%d=%d\t",a,b,a*b);
     }
+    printf("  \n");
+}
     return 0;
 }
