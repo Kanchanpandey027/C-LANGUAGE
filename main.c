26326de542015711d5a5238cb632fba71ba259f9
Kanchan Pandey <162719920+Kanchanpandey027@users.noreply.github.com>
2024-05-12 22:14:36 +0530

    Add files via upload

--- main.c
+++ main.c
@@ -3,32 +3,78 @@
 
 int main()
 {
-    int a[3]={1,5,1};
-    int i,j,num;
-    printf("input the number of element:3\n");
-    for(i=0;i<3;i++)
-    {
-         printf("element a[%d]=%d\n",i,a[i]);
-    }
-    printf("the unique element found in the array are:");
+    int a[3];
+    int i;
+    printf("Enter the array element a[%d]:");
     for(i=0;i<3;i++)
         {
-            int isunique=1;
-            for(j=0;j<3;j++)
-                {
-                    if(i!=j && a[i]==a[j])
-                        {
-                            isunique=0;
-                            break;
-                         }
-                }
-
-            if(isunique)
-                {
-                    printf("%d",a[i]);
-                }
-
+    scanf("%d",&a[i]);
+        }
+    if (a[0]==a[1] && a[1]==a[2])
+       {
+        printf("0,1 and 2 are same ");
+       }
+   else if(a[0]==a[1])
+       {
+       if(a[1]>a[2])
+       {
+           printf("0 and 1 are same and maximum");
+       }
+       else
+        {
+        printf("2 is maximum then 0 and 1\n");
+        printf("0 and 1 are same");
+         }
+       }
+       else if(a[1]==a[2])
+       {
+           if(a[2]>a[0])
+          {
+          printf("0 and 2 are same and maximum");
+           }
+           else
+           {
+            printf("0 is maximum then 1 and 2\n");
+            printf("1 and 2 are same");
+           }
+       }
+           else if(a[0]==a[2])
+           {
+               if (a[0]>a[1])
+               {
+                   printf("0 and 2 are same and maximum");
+               }
+            else {
+                printf("1 is maximum than 0 and 2\n");
+                printf("0 and 2 are same ");
             }
+          }
+          else{
+             if(a[0]==a[1])
+            {
+               if(a[0]>a[2])
+                {
+                  printf("0 is maximum than 1 and 2");
+               }
+              else{
+                 printf("2 is maximum than 0 and 1\n");
+
+              }
+            }
+            else
+                {
+                 if(a[1]>a[2])
+                 {
+                   printf("1 is maximum than 0 and 2");
+                 }
+            }
+            else
+            {
+                printf("2 is greater than 0 and 1");
+            }
+           }
+}
+}
 
     return 0;
 }
