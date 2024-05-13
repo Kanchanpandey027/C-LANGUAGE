622cc9477b4b35ae41825f7002a996fe58197b40
Kanchan Pandey <162719920+Kanchanpandey027@users.noreply.github.com>
2024-05-13 22:14:39 +0530

    Add files via upload

--- main.c
+++ main.c
@@ -3,78 +3,23 @@
 
 int main()
 {
-    int a[3];
-    int i;
-    printf("Enter the array element a[%d]:");
+    int i,j;
+    int a[3][3]={{1,2,3},{4,5,6},{7,8,9}};
     for(i=0;i<3;i++)
-        {
-    scanf("%d",&a[i]);
-        }
-    if (a[0]==a[1] && a[1]==a[2])
-       {
-        printf("0,1 and 2 are same ");
-       }
-   else if(a[0]==a[1])
-       {
-       if(a[1]>a[2])
-       {
-           printf("0 and 1 are same and maximum");
-       }
-       else
-        {
-        printf("2 is maximum then 0 and 1\n");
-        printf("0 and 1 are same");
-         }
-       }
-       else if(a[1]==a[2])
-       {
-           if(a[2]>a[0])
-          {
-          printf("0 and 2 are same and maximum");
-           }
-           else
-           {
-            printf("0 is maximum then 1 and 2\n");
-            printf("1 and 2 are same");
-           }
-       }
-           else if(a[0]==a[2])
-           {
-               if (a[0]>a[1])
-               {
-                   printf("0 and 2 are same and maximum");
-               }
-            else {
-                printf("1 is maximum than 0 and 2\n");
-                printf("0 and 2 are same ");
-            }
-          }
-          else{
-             if(a[0]==a[1])
+    {
+        for(j=0;j<3;j++)
+        printf("element a[%d][%d]=%d\n",i,j,a[i][j]);
+    }
+            for(i=0;i<3;i++)
             {
-               if(a[0]>a[2])
-                {
-                  printf("0 is maximum than 1 and 2");
-               }
-              else{
-                 printf("2 is maximum than 0 and 1\n");
+              for(j=0;j<3;j++)
 
-              }
-            }
-            else
-                {
-                 if(a[1]>a[2])
-                 {
-                   printf("1 is maximum than 0 and 2");
-                 }
-            }
-            else
             {
-                printf("2 is greater than 0 and 1");
+               printf("%d  ",a[i][j]);
             }
-           }
-}
-}
-
+   {
+           printf("\n");
+   }
+            }
     return 0;
 }
