80336ffd641a63bed4496cf5424e3301f19a0e46
Kanchan Pandey <162719920+Kanchanpandey027@users.noreply.github.com>
2024-05-15 20:26:51 +0530

    Add files via upload

--- main.c
+++ main.c
@@ -3,8 +3,10 @@
 
 int main()
 {
-    char greeting[8]={'W','e','l','c','o','m','e','\0'};
-    char gr[11]={'w','3','r','e','s','o','u','r','c','e','\0'};
-    printf("the string you entered is :%s,%s\n",greeting,gr);
+    char str[15]={'w','3','r','e','s','o','u','r','c','e','.','c','o','m','.','\0'};
+    int len;
+    len=strlen(str);
+
+    printf("length of string is : %d\n",len);
     return 0;
 }
